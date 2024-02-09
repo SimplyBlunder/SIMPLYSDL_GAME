d2e35069050679582cc641ec610ab5cb4adf421e
SimplyBlunder <SimplyBlunder@gmail.com>
2024-02-09 21:50:31 +0000

    Target Frame/Cap implemented..Standard speed lcoked to these too.

--- src/main.c
+++ src/main.c
@@ -3,11 +3,21 @@
 // #include <SDL2/SDL_events.h>
 // #include <SDL2/SDL_render.h>
 // #include <SDL2/SDL_video.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
 
+int last_frame_time = 0;
 int game_is_running = FALSE;
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+
+struct ball {
+  float x;
+  float y;
+  float width;
+  float height;
+} ball;
 
 int initialise_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -48,14 +58,41 @@ void process_input() {
 
 void setup() {
   // TODO:
+
+  ball.x = 20;
+  ball.y = 20;
+  ball.width = 15;
+  ball.height = 15;
 }
 
 void update() {
-  // TODO:
+  // Waste some time until reach the frame target time
+  // this should be changed later.. not nice solution.
+  while (!SDL_TICKS_PASSED(SDL_GetTicks(), last_frame_time + FRAME_TARGET_TIME))
+    ;
+
+  float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
+
+  last_frame_time = SDL_GetTicks();
+
+  ball.x += 70 * delta_time;
+  ball.y += 50 * delta_time;
 }
 
 void render() {
-  // TODO:
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  // draw a rectangle
+  SDL_Rect ball_rect = {(int)ball.x, (int)ball.y, (int)ball.width,
+                        (int)ball.height};
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderFillRect(renderer, &ball_rect);
+
+  // Start drawing objets here
+  // TWO buffers for swap
+  SDL_RenderPresent(renderer);
 }
 
 void destroy_window() {
